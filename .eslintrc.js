b5df56dcfc4aaf9130b4cd24fbadc3944bff5ad0
huijiawei <huijiawei@meituan.com>
2021-08-03 19:55:03 +0800

    fix: 修复代码问题

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    'no-console': 'error', //增加no-console
+    'react/react-in-jsx-scope': 'off', //关闭'react/react-in-jsx-scope'
+    'react/jsx-props-no-spreading': 'off', //关闭'react/jsx-props-no-spreading'
+    'react/state-in-constructor': 'off',
+    'import/no-cycle': 'off', //循环引用
+    'camelcase': 'off', //驼峰
+    'consistent-return': 'off', //函数有return返回
+    'array-callback-return': 'off', //函数有return返回
+}
